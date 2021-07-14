40d90639af702662b94cdb2570b53fdfd1ca742d
lifranca <lifranca@student.42sp.org.br>
2021-07-14 18:15:56 +0000

    Shell00 nota 10

--- shell00/ex05/git_ignore.sh
+++ shell00/ex05/git_ignore.sh
@@ -1,4 +1,7 @@
 #Escreva um shell script curto que retornará a lista de arquivos existentes ignorados
 #pelo seu repositório git atualmente presentes no seu repositório local.
 #!/bin/sh
-git status --ignored -s | grep '!!' | cut -c 4-
+#git ls-files -i -o --exclude-standard
+#git ls-files --others --ignored --exclude-standard
+#git status --ignored -s | grep '!!' | cut -c 4-
+git ls-files --others --ignored --exclude-standard
--- shell00backup/ex05/git_ignore.sh
+++
@@ -1,4 +0,0 @@
-#Escreva um shell script curto que retornará a lista de arquivos existentes ignorados
-#pelo seu repositório git atualmente presentes no seu repositório local.
-#!/bin/sh
-git status --ignored -s | grep '!!'
